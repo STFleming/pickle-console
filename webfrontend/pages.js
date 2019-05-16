511aaa8ce87c41d834e804506940c163189ba6f3
Shane Fleming <t-shflem@microsoft.com>
2019-05-16 23:25:59 +0100

    added the pattern matching game to the main loop

--- webfrontend/pages.js
+++ webfrontend/pages.js
@@ -1,1 +1,1 @@
-var pages = [ 'alphabet.html', 'dylan.html', 'simple_lines.html', 'measles.html', 'colour_mixer.html' ];
+var pages = [ 'pattern-match.html', 'alphabet.html', 'dylan.html', 'simple_lines.html', 'measles.html', 'colour_mixer.html'];
